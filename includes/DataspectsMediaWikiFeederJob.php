3f7833911d7c4df07f592d7e1a1ef552d8902d12
lexsulzer <lex@dataspects.com>
2019-04-06 15:32:49 -0700

    Prepared REST post to dataspects feeder endpoint

--- includes/DataspectsMediaWikiFeederJob.php
+++ includes/DataspectsMediaWikiFeederJob.php
@@ -13,17 +13,28 @@ class DataspectsMediaWikiFeederJob extends \Job {
     //var_dump($this->getTitle());
     $params = new \FauxRequest(
       array(
-        'action' => 'query',
-        'list' => 'allpages',
-        'apnamespace' => 0,
-        'aplimit' => 10,
-        'apprefix' => 'M'
+        'action' => 'browsebysubject',
+        'subject' => $this->getTitle()
       )
     );
     $api = new \ApiMain( $params );
     $api->execute();
     $data = $api->getResult()->getResultData();
-    var_dump($data);
+    var_dump($data["query"]);
   }
+  // Compile $data into $mongoDoc so that it fits mongodoc!
+  $req = \MWHttpRequest::factory("http://", ["method" => "post", "postData" => $mongoDoc], __METHOD__);
+  $req->setHeader("content-type", "application/json");
+  $req->setHeader("accept", "application/json");
+  //$req->setHeader("dataspects-api-key", "");
+
+  $status = $req->execute();
+  if($status->isOK()) {
+    $req->getContent();
+  } else {
+
+  }
+
+
 
 }
