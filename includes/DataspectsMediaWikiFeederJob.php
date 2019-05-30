499cfc9187ed9f8ae451e72d8e538b7f85150c25
lexsulzer <lex@dataspects.com>
2019-05-30 14:39:32 +0200

    Ready for tests re new/update page

--- includes/DataspectsMediaWikiFeederJob.php
+++ includes/DataspectsMediaWikiFeederJob.php
@@ -3,13 +3,41 @@
 namespace MediaWiki\Extension\DataspectsMediaWikiFeeder;
 
 class DataspectsMediaWikiFeederJob extends \Job {
+  // https://doc.wikimedia.org/mediawiki-core/master/php/classJob.html
+
+  private $annotations = [];
+  // FIXME: Why can't these instance variables be declared private?
+  // private $title = '';
+  // private $wikiPage = '';
+  // private $wikitext = '';
+  // private $parsedWikitext = '';
 
   public function __construct(\Title $title) {
+    // https://doc.wikimedia.org/mediawiki-core/master/php/classTitle.html
+    // https://www.mediawiki.org/wiki/Manual:Title.php#Functions
     parent::__construct("DataspectsMediaWikiFeederJob", []);
     $this->title = $title;
   }
 
   public function run() {
+    // https://doc.wikimedia.org/mediawiki-core/master/php/classWikiPage.html
+    // https://www.mediawiki.org/wiki/Manual:WikiPage.php
+    $this->wikiPage = \WikiPage::factory($this->title);
+    $this->getParsedWikitext();
+    $this->getAnnotations();
+    $this->sendToMongoDB();
+  }
+
+  private function getParsedWikitext() {
+    $revision = $this->wikiPage->getRevision();
+    $content = $revision->getContent( \Revision::RAW );
+    $this->wikitext = \ContentHandler::getContentText( $content );
+    $parser = new \Parser();
+    $parserOptions = new \ParserOptions();
+    $this->parsedWikitext = $parser->parse($this->wikitext, $this->title, $parserOptions);
+  }
+
+  private function getAnnotations() {
     $params = new \FauxRequest(
       array(
         'action' => 'browsebysubject',
@@ -19,51 +47,57 @@ class DataspectsMediaWikiFeederJob extends \Job {
     $api = new \ApiMain( $params );
     $api->execute();
     $data = $api->getResult()->getResultData();
-    // print_r(var_dump($this->getTitle()));
-    // print_r($data["query"]);
-    // var_dump($this->mongodoc());
-    $this->getAnnotations($data);
-    // $this->sendToMongoDB();
-  }
-
-  public function getAnnotations($data) {
-    foreach($data["query"]["data"] as $property) {
-      $propertyName = $property["property"];
-      if($propertyName[0] != "_") {
-        echo($propertyName);
+    foreach($data['query']['data'] as $property) {
+      if(is_array($property)) {
+        $propertyName = $property['property'];
+        if($propertyName[0] != '_') {
+          foreach($property['dataitem'] as $object) {
+            if(is_array($object)) {
+              $this->annotations[] = array(
+                'subject' => $this->getTitle()->prefixedText,
+                'predicate' => $propertyName,
+                'object' => array(
+                  'source' => str_replace('#0##', '', $object['item']),
+                  'html' => '',
+                  'text' => ''
+                )
+              );
+            }
+          }
+        }
       }
     }
   }
 
-  public function mongodoc() {
-    // $this->getTitle()->mTextform->prefixedText is "Template:MyName"
+  private function mongodoc() {
     $mongoDoc = array(
-      "slug" => "TEST",
-      "resourceSiloType" => "TEST2",
-      "resourceSiloLabel" => "TEST",
-      "resourceSiloID" => "TEST",
-      "resourceType" => "TEST",
+      "slug" => "pending",
+      "resourceSiloType" => "pending",
+      "resourceSiloLabel" => "pending",
+      "resourceSiloID" => "pending",
+      "resourceType" => "MediaWikiPage",
       "pagename" => $this->getTitle()->mTextform,
-      "rawUrl" => "TEST",
-      "shortUrl" => "TEST",
-      "namespace" => $this->getTitle()->mNamespace,
+      // Do we want the index.php?title= form here?
+      "rawUrl" => $this->title->getInternalURL(),
+      "shortUrl" => $this->title->getFullURL(),
+      "namespace" => $this->getNamespace($this->title->getNamespace()),
       "full" => array(
-        "wikitext" => "TEST",
-        "text" => "TEST",
-        "html" => "TEST"
+        "wikitext" => $this->wikitext,
+        "text" => "NOT USED BECAUSE NO TIKA HERE",
+        "html" => $this->parsedWikitext->mText
       ),
       "nonFormssemanticized" => array(
-        "html" => "TEST",
-        "text" => "TEST",
+        "html" => "pending",
+        "text" => "pending",
       ),
-      "annotations" => array(),
-      "feederClass" =>"dummy"
+      "annotations" => $this->annotations,
+      "feederClass" =>"DataspectsMediaWikiFeeder"
     );
     return json_encode($mongoDoc);
   }
 
-  public function sendToMongoDB() {
-    $url = $GLOBALS['wgDataspectsApiURL'].$GLOBALS['wgMediaWikiMongoID']."/pages";
+  private function sendToMongoDB() {
+    $url = $GLOBALS['wgDataspectsApiURL'].'mediawikis/'.$GLOBALS['wgMediaWikiMongoID']."/pages";
     // Compile $data into $mongoDoc so that it fits mongodoc!
     $req = \MWHttpRequest::factory(
       $url,
@@ -84,6 +118,15 @@ class DataspectsMediaWikiFeederJob extends \Job {
     } else {
       echo $status;
     }
+  }
+
+  private function getNamespace($index) {
+    if($index == 0) {
+      $namespace = 'Mainspace';
+    } else {
+      $namespace = \MWNamespace::getCanonicalName($index);
+    }
+    return $namespace;
   }
 
 }
